28127d6301c85f7f7a2a4ccb48347cc7b13d47c5
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2024-05-15 15:34:25 +1000

    Free Alexey Pertsev!

--- docs/datatransfer.js
+++ docs/datatransfer.js
@@ -49,7 +49,7 @@ function checkData(data) {
   if (["address", "keystore"].includes(type)) {
     try {
       const testAddress = ethers.utils.getAddress(data.address);
-      console.log("testAddress: " + testAddress);
+      // console.log("testAddress: " + testAddress);
     } catch (e) {
       error = "'address' invalid";
     }
@@ -101,29 +101,55 @@ function checkData(data) {
       error = "'crypto.mac' missing";
     }
   }
+  if (!error && ["signedtype1", "unsignedtype1", "signedtype2", "unsignedtype2"].includes(type)) {
+    try {
+      const testFrom = ethers.utils.getAddress(data.from);
+      console.log("testFrom: " + testFrom);
+    } catch (e) {
+      error = "'from' invalid";
+    }
+    if (!error) {
+      if (!('to' in data)) {
+        error = "'to' missing";
+      } else {
+        if (data.to) {
+          try {
+            const testTo = ethers.utils.getAddress(data.to);
+            console.log("testTo: " + testTo);
+          } catch (e) {
+            error = "'to' invalid";
+          }
+        }
+      }
+    }
 
-  // {
-  //   "address": "720169e755ba14017df473d9d10e682e1e41395a",
-  //   "id": "b4969a37-2d49-4190-bdb5-1987b2f02c21",
-  //   "version": 3,
-  //   "crypto": {
-  //     "cipher": "aes-128-ctr",
-  //     "cipherparams": {
-  //       "iv": "adb90f4d486c314e2ec08efbb5f3acac"
-  //     },
-  //     "ciphertext": "94e241c724d6eeabe900c4d400fee4d504c622e8b1f4432e75fe4dc261fcb05f",
-  //     "kdf": "scrypt",
-  //     "kdfparams": {
-  //       "salt": "fab44d0fe773999496d90a135fe2e5b5c6aef8aee67451c6de5e0efa9076f0e3",
-  //       "n": 131072,
-  //       "dklen": 32,
-  //       "p": 1,
-  //       "r": 8
-  //     },
-  //     "mac": "d4c16a7003d98d2d8f5962181f8c81a1b2d01f9a8c11553e886919967a3d02da"
-  //   },
-  // },
+    if (!error) {
+      if (!('value' in data)) {
+        error = "'value' missing";
+      } else {
+        try {
+          const testValue = ethers.BigNumber.from(data.value);
+          console.log("testValue: " + testValue);
+        } catch (e) {
+          error = "'value' invalid";
+        }
+      }
+    }
 
+    // {
+    //   "from": "0x4a7075B7D7E0bB80e8e6A0Fcf4fB6E1f33963F6B",
+    //   "to": "0x4a7075B7D7E0bB80e8e6A0Fcf4fB6E1f33963F6B",
+    //   "value": "1000000000000000000",
+    //   "data": "0x",
+    //   // "type": 1,
+    //   "chainId": 1,
+    //   "nonce": 1,
+    //   "gasLimit": "1",
+    //   "gasPrice": "1000000000",
+    //   "accessList": [],
+    // },
+
+  }
 
   console.log("checkData - type: " + type + ", error: " + error);
 
@@ -156,57 +182,57 @@ function testIt() {
     //   "address": "0x720169E755Ba14017DF473d9D10e682E1e41395", // Too short
     // },
 
+    // {
+    //   "address": "720169e755ba14017df473d9d10e682e1e41395a",
+    //   "id": "b4969a37-2d49-4190-bdb5-1987b2f02c21",
+    //   "version": 3,
+    //   "crypto": {
+    //     "cipher": "aes-128-ctr",
+    //     "cipherparams": {
+    //       "iv": "adb90f4d486c314e2ec08efbb5f3acac"
+    //     },
+    //     "ciphertext": "94e241c724d6eeabe900c4d400fee4d504c622e8b1f4432e75fe4dc261fcb05f",
+    //     "kdf": "scrypt",
+    //     "kdfparams": {
+    //       "salt": "fab44d0fe773999496d90a135fe2e5b5c6aef8aee67451c6de5e0efa9076f0e3",
+    //       "n": 131072,
+    //       "dklen": 32,
+    //       "p": 1,
+    //       "r": 8
+    //     },
+    //     "mac": "d4c16a7003d98d2d8f5962181f8c81a1b2d01f9a8c11553e886919967a3d02da"
+    //   },
+    // },
+
     {
-      "address": "720169e755ba14017df473d9d10e682e1e41395a",
-      "id": "b4969a37-2d49-4190-bdb5-1987b2f02c21",
-      "version": 3,
-      "crypto": {
-        "cipher": "aes-128-ctr",
-        "cipherparams": {
-          "iv": "adb90f4d486c314e2ec08efbb5f3acac"
-        },
-        "ciphertext": "94e241c724d6eeabe900c4d400fee4d504c622e8b1f4432e75fe4dc261fcb05f",
-        "kdf": "scrypt",
-        "kdfparams": {
-          "salt": "fab44d0fe773999496d90a135fe2e5b5c6aef8aee67451c6de5e0efa9076f0e3",
-          "n": 131072,
-          "dklen": 32,
-          "p": 1,
-          "r": 8
-        },
-        "mac": "d4c16a7003d98d2d8f5962181f8c81a1b2d01f9a8c11553e886919967a3d02da"
-      },
+      "from": "0x4a7075B7D7E0bB80e8e6A0Fcf4fB6E1f33963F6B",
+      "to": "0x4a7075B7D7E0bB80e8e6A0Fcf4fB6E1f33963F6B",
+      "value": "1000000000000000000",
+      "data": "0x",
+      // "type": 1,
+      "chainId": 1,
+      "nonce": 1,
+      "gasLimit": "1",
+      "gasPrice": "1000000000",
+      "accessList": [],
     },
 
-   //  {
-   //    "from": "0x4a7075B7D7E0bB80e8e6A0Fcf4fB6E1f33963F6B",
-   //    "to": "0x4a7075B7D7E0bB80e8e6A0Fcf4fB6E1f33963F6B",
-   //    "value": "1000000000000000000",
-   //    "data": "0x",
-   //    // "type": 1,
-   //    "chainId": 1,
-   //    "nonce": 1,
-   //    "gasLimit": "1",
-   //    "gasPrice": "1000000000",
-   //    "accessList": [],
-   // },
-
-   // {
-   //    "from": "0x4a7075B7D7E0bB80e8e6A0Fcf4fB6E1f33963F6B",
-   //    "to": "0x4a7075B7D7E0bB80e8e6A0Fcf4fB6E1f33963F6B",
-   //    "value": "1000000000000000000",
-   //    "data": "0x",
-   //    // "type": 1,
-   //    "chainId": 1,
-   //    "nonce": 1,
-   //    "gasLimit": "1",
-   //    "gasPrice": "1000000000",
-   //    "accessList": [],
-   //    "hash": "0x6aa502ae42111476faff7ad21ca7428e91de9f8050d2d8616eb870a811e1f9ce",
-   //    "v": 1,
-   //    "r": "0x02fa15d6d25494980949fa657019e0ad2bfae0ba15deb071a5857db2626988d8",
-   //    "s": "0x45c5f68151ac0ee1ada671a365104b01e4028d3cfa24bdb6f4c767efb8f15f72",
-   //  },
+    // {
+    //    "from": "0x4a7075B7D7E0bB80e8e6A0Fcf4fB6E1f33963F6B",
+    //    "to": "0x4a7075B7D7E0bB80e8e6A0Fcf4fB6E1f33963F6B",
+    //    "value": "1000000000000000000",
+    //    "data": "0x",
+    //    // "type": 1,
+    //    "chainId": 1,
+    //    "nonce": 1,
+    //    "gasLimit": "1",
+    //    "gasPrice": "1000000000",
+    //    "accessList": [],
+    //    "hash": "0x6aa502ae42111476faff7ad21ca7428e91de9f8050d2d8616eb870a811e1f9ce",
+    //    "v": 1,
+    //    "r": "0x02fa15d6d25494980949fa657019e0ad2bfae0ba15deb071a5857db2626988d8",
+    //    "s": "0x45c5f68151ac0ee1ada671a365104b01e4028d3cfa24bdb6f4c767efb8f15f72",
+    //  },
 
     // {
     //   "from": "0x4a7075B7D7E0bB80e8e6A0Fcf4fB6E1f33963F6B",
