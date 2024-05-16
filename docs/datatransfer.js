3647479f83bec00082a0d487f7d9c00c9cc4f682
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2024-05-16 11:14:42 +1000

    Free Pertsev!

--- docs/datatransfer.js
+++ docs/datatransfer.js
@@ -19,7 +19,7 @@ function dataToTransfer(type, data) {
 function dataType(data) {
   if (data) {
     if ('address' in data) {
-      if (data.crypto) {
+      if ('crypto' in data) {
         return "keystore";
       } else {
         return "address";
@@ -314,9 +314,9 @@ function testIt() {
     // {
     //   "address": "720169e755ba14017df473d9d10e682e1e41395a", // No 0x prefix
     // },
-    // {
-    //   "address": "0x720169E755Ba14017DF473d9D10e682E1e41395A", // 0x prefix
-    // },
+    {
+      "address": "0x720169E755Ba14017DF473d9D10e682E1e41395A", // 0x prefix
+    },
     // {
     //   "address": "0x720169E755Ba14017DF473d9D10e682E1e41395a", // Incorrect case
     // },
@@ -327,27 +327,27 @@ function testIt() {
     //   "address": "0x720169E755Ba14017DF473d9D10e682E1e41395", // Too short
     // },
 
-    // {
-    //   "address": "720169e755ba14017df473d9d10e682e1e41395a",
-    //   "id": "b4969a37-2d49-4190-bdb5-1987b2f02c21",
-    //   "version": 3,
-    //   "crypto": {
-    //     "cipher": "aes-128-ctr",
-    //     "cipherparams": {
-    //       "iv": "adb90f4d486c314e2ec08efbb5f3acac"
-    //     },
-    //     "ciphertext": "94e241c724d6eeabe900c4d400fee4d504c622e8b1f4432e75fe4dc261fcb05f",
-    //     "kdf": "scrypt",
-    //     "kdfparams": {
-    //       "salt": "fab44d0fe773999496d90a135fe2e5b5c6aef8aee67451c6de5e0efa9076f0e3",
-    //       "n": 131072,
-    //       "dklen": 32,
-    //       "p": 1,
-    //       "r": 8
-    //     },
-    //     "mac": "d4c16a7003d98d2d8f5962181f8c81a1b2d01f9a8c11553e886919967a3d02da"
-    //   },
-    // },
+    {
+      "address": "720169e755ba14017df473d9d10e682e1e41395a",
+      "id": "b4969a37-2d49-4190-bdb5-1987b2f02c21",
+      "version": 3,
+      "crypto": {
+        "cipher": "aes-128-ctr",
+        "cipherparams": {
+          "iv": "adb90f4d486c314e2ec08efbb5f3acac"
+        },
+        "ciphertext": "94e241c724d6eeabe900c4d400fee4d504c622e8b1f4432e75fe4dc261fcb05f",
+        "kdf": "scrypt",
+        "kdfparams": {
+          "salt": "fab44d0fe773999496d90a135fe2e5b5c6aef8aee67451c6de5e0efa9076f0e3",
+          "n": 131072,
+          "dklen": 32,
+          "p": 1,
+          "r": 8
+        },
+        "mac": "d4c16a7003d98d2d8f5962181f8c81a1b2d01f9a8c11553e886919967a3d02da"
+      },
+    },
 
     // {
     //   "from": "0x4a7075B7D7E0bB80e8e6A0Fcf4fB6E1f33963F6B",
@@ -508,5 +508,5 @@ async function testItNew() {
 }
 
 
-// testIt();
-testItNew();
+testIt();
+// testItNew();
